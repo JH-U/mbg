473303a5cd8a58337f48579dffc99d50555ed0de
zz <dalao@qq.com>
2021-01-09 00:04:32 +0800

    Hello.java

---
+++ src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @Date:2021/1/9 -0:03
+ * @Auther:浮生
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
