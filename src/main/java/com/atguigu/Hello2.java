19eebfcfdae55171110c0d806cbf246c12d07c09
zz <dalao@qq.com>
2021-01-09 00:06:16 +0800

    Hello.java2

---
+++ src/main/java/com/atguigu/Hello2.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @Date:2021/1/9 -0:05
+ * @Auther:浮生
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("helloworld2");
+    }
+}
